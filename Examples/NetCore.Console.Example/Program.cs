9471262398d53af7b69e2e696f8ed3b51bf0575c
juanal98 <32484496+juanal98@users.noreply.github.com>
2021-04-02 21:22:52 +0200

    Added net core example

---
+++ Examples/NetCore.Console.Example/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace NetCore.Console.Example
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            //
+            //
+            //  This is an example to use Shield client in .NET Core app with async methods.
+            // 
+            //
+
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            await using var serviceProvider = services.BuildServiceProvider();
+            var app = serviceProvider.GetService<Consumer>();
+            // ReSharper disable once PossibleNullReferenceException
+            await app?.RunWithQueues();
+        }
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddConsole().SetMinimumLevel(LogLevel.Debug))
+                .AddTransient<Consumer>();
+        }
+    }
+}
