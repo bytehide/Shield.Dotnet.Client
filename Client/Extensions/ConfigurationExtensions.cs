1b9c6156779a5a899967128bfd06e891f8ce5449
juanal98 <32484496+juanal98@users.noreply.github.com>
2021-04-19 23:24:14 +0200

    fixed invalid dollar

--- Client/Extensions/ConfigurationExtensions.cs
+++ Client/Extensions/ConfigurationExtensions.cs
@@ -11,13 +11,13 @@ namespace Shield.Client.Extensions
         //    => await File.WriteAllTextAsync(Path.Combine(path, $"shield.application.${applicationName}.json"), JsonConvert.SerializeObject(config));
 
         public static void SaveToFile(this ApplicationConfigurationDto config, string path, string applicationName)
-            =>  File.WriteAllText(Path.Combine(path, $"shield.application.${applicationName}.json"), JsonConvert.SerializeObject(config));
+            =>  File.WriteAllText(Path.Combine(path, $"shield.application.{applicationName}.json"), JsonConvert.SerializeObject(config));
 
         //public static async Task SaveToFileAsync(this ProjectConfigurationDto config, string path, string projectName)
         //    => await File.WriteAllTextAsync(Path.Combine(path,$"shield.project.${projectName}.json"), JsonConvert.SerializeObject(config));
 
         public static void SaveToFile(this ProjectConfigurationDto config, string path, string projectName)
-            =>  File.WriteAllText(Path.Combine(path, $"shield.project.${projectName}.json"), JsonConvert.SerializeObject(config));
+            =>  File.WriteAllText(Path.Combine(path, $"shield.project.{projectName}.json"), JsonConvert.SerializeObject(config));
 
     }
 }
