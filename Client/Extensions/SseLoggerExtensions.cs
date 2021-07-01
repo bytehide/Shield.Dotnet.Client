8283bdf4114b65621a6e577385459f1b1d71ed58
juanal98 <32484496+juanal98@users.noreply.github.com>
2021-07-01 20:49:21 +0200

    fixed typo in logger extensions

--- Client/Extensions/SseLoggerExtensions.cs
+++ Client/Extensions/SseLoggerExtensions.cs
@@ -12,14 +12,14 @@ namespace Shield.Client.Extensions
     /// </summary>
     public static class SseLoggerExtensions
     {
-        public static void OnSuccess(this ServerSentEvents connection, Action<ProtectedApplicationDto> action)
+        public static void WhereSuccess(this ServerSentEvents connection, Action<ProtectedApplicationDto> action)
             => connection.On("onSuccess", (message, level, time) => action(JsonSerializer.Deserialize<ProtectedApplicationDto>(message)));
-        public static void OnError(this ServerSentEvents connection, Action<string> action)
+        public static void WhereError(this ServerSentEvents connection, Action<string> action)
             => connection.On("onError", (message, level, time) => action(message));
-        public static void OnClose(this ServerSentEvents connection, Action<string> action)
-            => connection.On("onClose", (message, level, time) => action(message));
+        public static void WhereClose(this ServerSentEvents connection, Action action)
+            => connection.On("onClose", (message, level, time) => action?.Invoke());
 
-        public static void OnLog(this ServerSentEvents connection, string id, Action<string, string, string> action)
+        public static void WhereLog(this ServerSentEvents connection, string id, Action<string, string, string> action)
             => connection.On(id, (message, level, time) => action(time.ToString(CultureInfo.InvariantCulture), message, level));
     }
 }
