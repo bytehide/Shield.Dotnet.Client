38baad5d669d8bdaa7abe522bc093c1dd9cd931d
Juan España Garcia <juanal98@hotmail.com>
2023-03-15 20:32:42 +0000

    added new configuration file support beta WIP

--- Examples/NetCore.Console.Example/Consumer.cs
+++ Examples/NetCore.Console.Example/Consumer.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Shield.Client;
 using Shield.Client.Extensions;
+using Shield.Client.Models.API.Application;
+using RestSharp.Serialization.Json;
 
 namespace NetCore.Console.Example
 {
@@ -22,16 +24,16 @@ namespace NetCore.Console.Example
             _logger.LogInformation("Application Started at {dateTime}", DateTime.UtcNow);
 
             const string directory =
-                @"C:\Users\juana\source\repos\Ejemplo\bin\Debug\netcoreapp3.1\";
+                @"C:\Users\juan\source\repos\ExceptionsApps\ExceptionsApps\bin\Debug\net7.0";
 
-            var appPath = $"{directory}Ejemplo.dll";
+            var appPath = $"{directory}\\FlappyBit Modder.exe";
 
-            var save = $"{directory}Ejemplo_protegido.dll";
+            var save = $"{directory}\\FlappyBit Modder_protegido.dll";
 
             var dependencies = Directory.GetFiles($"{directory}").ToList();
 
             var client = ShieldClient.CreateInstance(
-                "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5NDNmMGMwMS0yOTBiLTQ4NjYtYTAyZi1lN2Q4NTU2OWE5OTQiLCJqdGkiOiIxMGU5MzU5NDBhOGFjOGI4MDQyMzVkYzI0ZmY2NmJkMmEwODZmMmNhNmI0NWFhNDkwMzQ5YTNiZDcyMzQzZmRiM2RiZmU3Zjk0ODFkMGJlMCIsImlhdCI6MTYzMzQzMzE0My43Mjk3NTUsIm5iZiI6MTYzMzQzMzE0My43Mjk3NTksImV4cCI6MTY2NDk2OTE0My42ODg1MSwic3ViIjoiOTQ1OTc5NTgtY2JjYi00OThhLTllNTAtYzQ0NmUzYTIyZDkyIiwic2NvcGVzIjpbXX0.a7MTfyKDV6TrLg9r-fpn0qTazcWGzBM1uzZiOjB2YT-ex_fEIZlWs0gO7TbTVQbs3E9LuLO-HNQz5Q0VQeN9gW1tfW-aB5mIN0T2FNMeECcqT2UQeVD5W1AGU3K4oadOMUMif9Hqujgyi3mXU4zt_EubHZ-A8eLhUDcbP5UdLJE7FeYnyCHLXM4RbWgzugqRMmwzt6qctQ_uPXLfdiSL6Br0Cb5mA45tBwU2hcWfN3wLeCIeIncN1HxOpBBphMjOLP27N39OMf0uFJ4eqvnWC7AM_aEsWGCuse9svxRYSeznFVoU9KaPu_X9geRbBBat35QnDjnzJyHOa30EiK-qnuRlrAX-Qhrm1LTGx0hQY461Yx7hv2tnlT-s2_s41w02DSAlxQsNKrxAH5U1OKUujgLEJNsEONRnD7oYBuu2NisRFg8RWfJi3S6kZS8UjrXR646J7AkBB8R2EUeJULEv1SUoDLTveCl1NyDJQjvhFroQrhKsuT4QTH2Gd8kWcjEfRrm63qjzFMLGDo7JcMCh_O4lXNXdJ6Bk8T3eWdCk1vfUoPeiZ9ppuYtR4XncuNk-93sb3FchU-dNZGlcrq4LEcyn1lNMflCSARfqdbvdwn-4QYu7X03nfZqmQ2mzxmr7aUOu1z2idpvACKKaPJ-G9akWOzfVYPKgH0WA0XH81zE"
+                "9K5elBOWEfe35gPB7Lg3IjKtHBrzYFwFzepo92ZJ"
                 , _logger);
 
             var projectTest = await client.Project.FindOrCreateExternalProjectAsync("arg3");
@@ -79,21 +81,20 @@ namespace NetCore.Console.Example
         {
             _logger.LogInformation("Application Started at {dateTime}", DateTime.UtcNow);
 
-            var client = ShieldClient.CreateInstance(
-                "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5NDNmMGMwMS0yOTBiLTQ4NjYtYTAyZi1lN2Q4NTU2OWE5OTQiLCJqdGkiOiIxMGU5MzU5NDBhOGFjOGI4MDQyMzVkYzI0ZmY2NmJkMmEwODZmMmNhNmI0NWFhNDkwMzQ5YTNiZDcyMzQzZmRiM2RiZmU3Zjk0ODFkMGJlMCIsImlhdCI6MTYzMzQzMzE0My43Mjk3NTUsIm5iZiI6MTYzMzQzMzE0My43Mjk3NTksImV4cCI6MTY2NDk2OTE0My42ODg1MSwic3ViIjoiOTQ1OTc5NTgtY2JjYi00OThhLTllNTAtYzQ0NmUzYTIyZDkyIiwic2NvcGVzIjpbXX0.a7MTfyKDV6TrLg9r-fpn0qTazcWGzBM1uzZiOjB2YT-ex_fEIZlWs0gO7TbTVQbs3E9LuLO-HNQz5Q0VQeN9gW1tfW-aB5mIN0T2FNMeECcqT2UQeVD5W1AGU3K4oadOMUMif9Hqujgyi3mXU4zt_EubHZ-A8eLhUDcbP5UdLJE7FeYnyCHLXM4RbWgzugqRMmwzt6qctQ_uPXLfdiSL6Br0Cb5mA45tBwU2hcWfN3wLeCIeIncN1HxOpBBphMjOLP27N39OMf0uFJ4eqvnWC7AM_aEsWGCuse9svxRYSeznFVoU9KaPu_X9geRbBBat35QnDjnzJyHOa30EiK-qnuRlrAX-Qhrm1LTGx0hQY461Yx7hv2tnlT-s2_s41w02DSAlxQsNKrxAH5U1OKUujgLEJNsEONRnD7oYBuu2NisRFg8RWfJi3S6kZS8UjrXR646J7AkBB8R2EUeJULEv1SUoDLTveCl1NyDJQjvhFroQrhKsuT4QTH2Gd8kWcjEfRrm63qjzFMLGDo7JcMCh_O4lXNXdJ6Bk8T3eWdCk1vfUoPeiZ9ppuYtR4XncuNk-93sb3FchU-dNZGlcrq4LEcyn1lNMflCSARfqdbvdwn-4QYu7X03nfZqmQ2mzxmr7aUOu1z2idpvACKKaPJ-G9akWOzfVYPKgH0WA0XH81zE"
-                , _logger);
-
-
             const string directory =
-                @"C:\Users\juana\source\repos\Ornitorrinco\Ornitorrinco\bin\Debug\netcoreapp3.1\";
+               @"C:\Users\juan\source\repos\ExceptionsApps\ExceptionsApps\bin\Debug\net7.0";
 
-            var appPath = $"{directory}Ornitorrinco.dll";
+            var appPath = $"{directory}\\FlappyBit Modder.exe";
 
-            var save = $"{directory}Ornitorrinco_protegida.dll";
+            var save = $"{directory}\\FlappyBit Modder_protegido.dll";
 
             var dependencies = Directory.GetFiles($"{directory}").ToList();
 
-           
+            var client = ShieldClient.CreateInstance(
+                "9K5elBOWEfe35gPB7Lg3IjKtHBrzYFwFzepo92ZJ"
+                , _logger);
+
+
             var projectTest = await client.Project.FindOrCreateExternalProjectAsync("hola");
 
             var uploadApplicationDirectly = await client.Application.UploadApplicationDirectlyAsync(projectTest.Key, appPath, null);
@@ -111,10 +112,21 @@ namespace NetCore.Console.Example
             var appBlob = uploadApplicationDirectly.ApplicationBlob;
 
 
-            var config = client.Configuration.FindApplicationConfiguration(
-                             directory,
-                             Path.GetFileName(appPath)) ??
-                         client.Configuration.MakeApplicationCustomConfiguration("consts mutation");
+            var config = new ProtectionConfigurationDTO
+            {
+                ConfigurationType = ConfigurationType.Application,
+                Preset = "custom",
+                Protections = new System.Collections.Generic.Dictionary<string, System.Collections.Generic.Dictionary<string, object>>
+                {
+                    { "rename", new System.Collections.Generic.Dictionary<string, object> { { "rename public", true} } }
+                }
+            };
+
+
+                //client.Configuration.FindApplicationConfiguration(
+                //             directory,
+                //             Path.GetFileName(appPath)) ??
+                //         client.Configuration.MakeApplicationCustomConfiguration("consts mutation");
 
             var taskConnection = client.Connector.InstanceSseConnectorWithLogger();
 
